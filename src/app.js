66a885265c483927047e6c51a05b3108929d01ef
Gideon <tranminhquan.dtp1b@gmail.com>
2026-09-18 13:53:40 +0700

    scaffold project structure

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use('/', routes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not Found' });
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ error: err.message || 'Internal Server Error' });
+});
+
+module.exports = app;
